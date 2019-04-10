9fab4e84466facffcea7b5bdbe937eabb3aec85d
Saurav Azad <saurav.azad@quifers.com>
2019-04-10 22:18:25 +0530

    Updates

---
+++ precache-manifest.bec4ab006b3190e70885ebbbaf7cce37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b80cb36560d3f5f41df9",
+    "url": "/goodreads/static/js/runtime~main.9ef7e3d0.js"
+  },
+  {
+    "revision": "ff64b254151b50fce7c4",
+    "url": "/goodreads/static/js/main.e81334ec.chunk.js"
+  },
+  {
+    "revision": "4d0c07a183ca9e23b7a3",
+    "url": "/goodreads/static/js/2.6d6f61c0.chunk.js"
+  },
+  {
+    "revision": "ff64b254151b50fce7c4",
+    "url": "/goodreads/static/css/main.c8a7af2b.chunk.css"
+  },
+  {
+    "revision": "b1370c617cdf924167c2451c3a16dc72",
+    "url": "/goodreads/index.html"
+  }
+];
